779565b052d7570e046a5703ebef4b0d0d75db55
zomb <logankmet@gmail.com>
2025-09-16 10:24:38 -0700

    er45y

--- Faithful/Assets/Scripts/PlayerController.cs
+++ Faithful/Assets/Scripts/PlayerController.cs
@@ -10,20 +10,17 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
-    Vector2 cameraRotation;
-    InputAction lookVector;
-    Transform playerCam;
-    Vector3 cameraOffset;
+    Camera playerCam;
     public Vector3 respawn;
+    public float dist = .5f; //groundDetectionLength
+    Ray ray;
+    RaycastHit hit;
 
     public float ForwardBackNow; //vert
     public float SidewaysNow; //horiz
 
     public float ishowspeed = 10f; //you'll never guess
     public float howMuchBoof = 10f; //jump power
-    public float XLibSensitivity = 0.5f;
-    public float YRetSensitivity = 0.5f;
-    public float rotationalLimitations = 90.0f;
 
     public int healthy = 5;
     public int maxHealthy = 10;
@@ -31,11 +28,9 @@ public class PlayerController : MonoBehaviour
     public void Start()
     {
         respawn = new Vector3(0, 1, 0);
-        cameraOffset = new Vector3(0, .5f, .15f);
         rb = GetComponent<Rigidbody>();
-        playerCam = transform.GetChild(0);
-        lookVector = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
-        cameraRotation = Vector2.zero;
+        playerCam = Camera.main;
+        ray = new Ray(transform.position, transform.forward);
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -53,21 +48,20 @@ public class PlayerController : MonoBehaviour
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Reloads scene
 
         //this moves my camera
-        //playerCam.transform.position = transform.position + cameraOffset;
-
-        cameraRotation.x += lookVector.ReadValue<Vector2>().x * XLibSensitivity;
-        cameraRotation.y += lookVector.ReadValue<Vector2>().y * YRetSensitivity;
-
-        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -rotationalLimitations, rotationalLimitations);
-
-        playerCam.transform.rotation = Quaternion.Euler(-cameraRotation.y, cameraRotation.x, 0);
-        transform.localRotation = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+        Quaternion playerRotation = playerCam.transform.rotation;
+        playerRotation.x = 0;
+        playerRotation.z = 0;
+        transform.localRotation = playerRotation;
 
         //bro move
         Vector3 temp = rb.linearVelocity;
 
         temp.x = ForwardBackNow * ishowspeed;
         temp.z = SidewaysNow * ishowspeed;
+
+        //bro jump
+        ray.origin = transform.position;
+        ray.direction = -transform.up;
 
         rb.linearVelocity = (temp.x * transform.forward) + (temp.y * transform.up) + (temp.z * transform.right);
     }
@@ -78,7 +72,11 @@ public class PlayerController : MonoBehaviour
         ForwardBackNow = inputAxis.y;
         SidewaysNow = inputAxis.x;
     }
-
+    public void Jump() //take another wild fucking guess
+    {
+        if (Physics.Raycast(ray,dist))
+            rb.AddForce(transform.up * howMuchBoof);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "InstantDeath")
