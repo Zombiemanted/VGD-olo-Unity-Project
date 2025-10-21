872b20e58090bf80cee352cd4cc74e15de2e23c6
zomb <logankmet@gmail.com>
2025-10-21 09:50:29 -0700

    OPlayermnoateds

--- Faithful/Assets/Scripts/BasicEnemyController.cs
+++ Faithful/Assets/Scripts/BasicEnemyController.cs
@@ -1,26 +1,60 @@
-using NUnit.Framework.Constraints;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using Unity.Collections;
-using Unity.Hierarchy;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
 using UnityEngine.AI;
 
 public class BasicEnemyController : MonoBehaviour
 {
-    //Script is a lie this is just my bullethell controller (Using a waypoint system itll send out Things.. Dodge them)
+    PlayerController player;
+    GameManager gm;
     Animator myAnim;
+
+    [Header("Logic")]
+    NavMeshAgent agent;
+    public bool isFollowing = false;
+
+    [Header("Enemy Stats")]
+    public int health = 5;
+    public int maxHealth = 5;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         myAnim = GetComponent<Animator>();
-        //if myAnim.SetBool("isAttacking", true); else stop agent myAnim.SetBool("isAttacking",false);
+        gm = GameObject.FindGameObjectWithTag("gm").GetComponent<GameManager>();
     }
 
+    // Update is called once per frame
     void Update()
     {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
 
+        else
+        {
+            if (isFollowing)
+            {
+                agent.isStopped = false;
+                agent.destination = player.transform.position;
+                myAnim.SetBool("isAttacking", true);
+            }
+            else
+            {
+                agent.isStopped = true;
+                myAnim.SetBool("isAttacking", false);
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "proj")
+        {
+            health--;
+            Destroy(collision.gameObject);
+        }
     }
 }
